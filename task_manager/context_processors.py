236a84711fcb5fe95f1c26b5662b391510730ff1
Petro <petropavliuk12.05@gmail.com>
2026-08-04 21:00:07 +0300

    added context_processors and updated settings.py for static files

---
+++ task_manager/context_processors.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+
+def assets_root(request):
+    return {
+        "ASSETS_ROOT": settings.ASSETS_ROOT,
+    }
--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-3848@k6%tu2p4d(dsfi!&4#k^_(req6suz5^&h&#^1%-fl5s_+
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -63,13 +61,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "task_manager.context_processors.assets_root",
             ],
         },
     },
 ]
 
 WSGI_APPLICATION = 'task_manager_service.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -80,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -112,12 +108,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-AUTH_USER_MODEL = 'task_manager.Worker'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
-STATIC_ROOT = BASE_DIR / 'static'
+AUTH_USER_MODEL = 'task_manager.Worker'
