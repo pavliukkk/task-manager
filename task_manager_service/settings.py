64521677bed9a94929e2cb211ce2024a7d95265e
Petro <petropavliuk12.05@gmail.com>
2026-08-04 17:43:13 +0300

    add user model to settings.py and changed timezone

--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kyiv'
 
 USE_I18N = True
 
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'task_manager.Worker'
+
+STATIC_ROOT = BASE_DIR / 'static'
