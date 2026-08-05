ca4b126a0e958dac15dc6fbf98108f9e40698aeb
Petro <petropavliuk12.05@gmail.com>
2026-08-05 20:59:44 +0300

    added url to index file, updated views.py and index.html for count all items in each model

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from task_manager.models import Worker, Position, Task, TaskType
+
+
 # Create your views here.
 def index(request):
-    return render(request, "task_manager/index.html")
+    num_workers = Worker.objects.count()
+    num_positions = Position.objects.count()
+    num_tasks = Task.objects.count()
+    num_task_types = TaskType.objects.count()
+    context = {
+        "num_workers": num_workers,
+        "num_positions": num_positions,
+        "num_tasks": num_tasks,
+        "num_task_types": num_task_types,
+    }
+    return render(request, "task_manager/index.html", context=context)
