643d51fe137e5fa3e1824d7a53a713516377c4ee
Petro <petropavliuk12.05@gmail.com>
2026-08-11 22:33:37 +0300

    changed imports in urls.py and admin.py to absolute imports

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Worker, Task, TaskType, Position
+from task_manager.models import Worker, Task, TaskType, Position
 
 
 admin.site.register(Worker)
