442a00a7b0342b3d1bba292fc40d64711bd97f78
Petro <petropavliuk12.05@gmail.com>
2026-08-13 20:54:13 +0300

    changed base.py, dev.py and manage.py

--- task_manager_service/settings/base.py
+++ task_manager_service/settings/base.py
@@ -16,18 +16,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ["SECRET_KEY"]
-
-# SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
-
-ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -73,16 +68,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "task_manager_service.wsgi.application"
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
